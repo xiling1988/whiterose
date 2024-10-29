7725ea8a68fe4fb0a0ef7ddcc9d2da1f4044565a
hans schilling <hasp1988@gmail.com>
2024-10-29 19:48:44 +0400

    test2

---
+++ schemaTypes/youtube/YoutubePreview 2.js
@@ -0,0 +1,16 @@
+// import {PreviewProps} from 'sanity'
+import { Flex, Text } from '@sanity/ui'
+import YouTubePlayer from 'react-player/youtube'
+
+export function YouTubePreview(props) {
+  const { title: url } = props
+  return (
+    <Flex padding={3} align='center' justify='center'>
+      {typeof url === 'string' ? (
+        <YouTubePlayer url={url} />
+      ) : (
+        <Text>Add a YouTube URL</Text>
+      )}
+    </Flex>
+  )
+}
