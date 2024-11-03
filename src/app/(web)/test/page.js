fcf8ed4e569689b858a882ab1ed746107818ef5e
hans schilling <hasp1988@gmail.com>
2024-11-03 17:52:00 +0400

    isr test

--- src/app/(web)/test/page.js
+++ src/app/(web)/test/page.js
@@ -1,7 +1,17 @@
+import { initializeSanityClient } from '@/libs/sanity'
+
 export const revalidate = 86400
 
-const page = () => {
-  return <div>This is a test page with ISR enabled.</div>
+const page = async () => {
+  const client = await initializeSanityClient()
+
+  const minimalData = await client.fetch(`*[_type == "heroImage"]{ _id }`, {
+    next: { revalidate: 86400 },
+  })
+
+  console.log('Minimal Data:', minimalData) // Log for debugging
+
+  return <div>Minimal data fetch with ISR enabled.</div>
 }
 
 export default page
