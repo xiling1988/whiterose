a5d9cd37e7b6a59dfd64f49e88e0b61ceaa52469
hans schilling <hasp1988@gmail.com>
2024-11-03 14:59:44 +0400

    removed fetch for test

--- src/app/(web)/page.js
+++ src/app/(web)/page.js
@@ -5,31 +5,31 @@ import Portfolio from '@/components/Sections/Portfolio'
 import Contact from '@/components/Sections/Contact'
 import { initializeSanityClient } from '@/libs/sanity'
 
-export const revalidate = 86400 // Revalidate every hour
+// export const revalidate = 86400 // Revalidate every hour
 
-export default async function Home() {
-  const client = await initializeSanityClient()
-  const heroImages = await client.fetch(
-    `*[_type == "heroImage"] | order(orderRank){
-      _id,
-      altText,
-      "imageUrl": image.asset->url
-    }`
-  )
+// export default async function Home() {
+//   const client = await initializeSanityClient()
+//   const heroImages = await client.fetch(
+//     `*[_type == "heroImage"] | order(orderRank){
+//       _id,
+//       altText,
+//       "imageUrl": image.asset->url
+//     }`
+//   )
 
-  const services = await client.fetch(
-    `*[_type == "service"] | order(orderRank){
-      title,
-      description,
-      "imageUrl": image.asset->url,
-      altText
-    }`
-  )
+//   const services = await client.fetch(
+//     `*[_type == "service"] | order(orderRank){
+//       title,
+//       description,
+//       "imageUrl": image.asset->url,
+//       altText
+//     }`
+//   )
 
   return (
     <>
-      <Hero images={heroImages} />
-      <Services services={services} />
+      {/* <Hero images={heroImages} />
+      <Services services={services} /> */}
       {/* <Portfolio /> */}
       <Contact />
     </>
