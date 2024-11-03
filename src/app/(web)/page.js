03b4a89a6543798ba9d464c135a6a56da3af22be
hans schilling <hasp1988@gmail.com>
2024-11-03 17:07:37 +0400

    changin revalidate

--- src/app/(web)/page.js
+++ src/app/(web)/page.js
@@ -5,7 +5,7 @@ import Portfolio from '@/components/Sections/Portfolio'
 import Contact from '@/components/Sections/Contact'
 import { initializeSanityClient } from '@/libs/sanity'
 
-export const revalidate = 86400 // Revalidate every hour
+// export const revalidate = 86400 // Revalidate every hour
 
 export default async function Home() {
   const client = await initializeSanityClient()
@@ -14,7 +14,8 @@ export default async function Home() {
       _id,
       altText,
       "imageUrl": image.asset->url
-    }`
+    }`,
+    { next: { revalidate: 86400 } }
   )
 
   // const services = await client.fetch(
