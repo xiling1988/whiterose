a558585ae95dd618faad06593cbe42077546786e
hans schilling <hasp1988@gmail.com>
2024-11-04 10:11:24 +0400

    footer change

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,11 +1,8 @@
 const navigation = {
   main: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Accessibility', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: 'Home', href: '#' },
+    { name: 'Services', href: '#services' },
+    { name: 'Portfolio', href: '#portfolio' },
   ],
   social: [
     {
@@ -35,28 +32,6 @@ const navigation = {
       ),
     },
     {
-      name: 'X',
-      href: '#',
-      icon: (props) => (
-        <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
-          <path d='M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z' />
-        </svg>
-      ),
-    },
-    {
-      name: 'GitHub',
-      href: '#',
-      icon: (props) => (
-        <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
-          <path
-            fillRule='evenodd'
-            d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
-            clipRule='evenodd'
-          />
-        </svg>
-      ),
-    },
-    {
       name: 'YouTube',
       href: '#',
       icon: (props) => (
@@ -75,7 +50,7 @@ const navigation = {
 export default function Footer() {
   const year = new Date().getFullYear()
   return (
-    <footer className='bg-nexusBlue'>
+    <footer className='bg-cream'>
       <div className='mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8'>
         <nav
           className='-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12'
@@ -85,7 +60,7 @@ export default function Footer() {
             <div key={item.name} className='pb-6'>
               <a
                 href={item.href}
-                className='text-sm leading-6 text-gray-200 hover:text-nexusRed'
+                className='text-sm leading-6 text-canvas-3 hover:text-canvas-1'
               >
                 {item.name}
               </a>
@@ -97,14 +72,14 @@ export default function Footer() {
             <a
               key={item.name}
               href={item.href}
-              className='text-nexusRed hover:text-gray-200'
+              className='text-canvas-3 hover:text-canvas-1'
             >
               <span className='sr-only'>{item.name}</span>
               <item.icon className='h-6 w-6' aria-hidden='true' />
             </a>
           ))}
         </div>
-        <p className='mt-10 text-center text-xs leading-5 text-gray-500'>
+        <p className='mt-10 text-center text-xs leading-5 text-canvas-3'>
           &copy; {year} White Rose Design. All rights reserved.
         </p>
         <p className='text-center mt-8 text-xs'>
@@ -115,7 +90,7 @@ export default function Footer() {
             viewBox='0 0 24 24'
             strokeWidth='1.5'
             stroke='currentColor'
-            className='inline text-nexusRed w-6 h-6'
+            className='inline text-canvas-3 w-6 h-6'
           >
             <path
               strokeLinecap='round'
@@ -127,7 +102,7 @@ export default function Footer() {
           <a
             target='_blank'
             href='https://pottwalsolutions.com/'
-            className='underline text-nexusRed'
+            className='underline text-canvas-3'
           >
             Pottwal Solutions
           </a>
