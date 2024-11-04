fc58d5a0146c5e541fc7f572880a1c04e838da64
hans schilling <hasp1988@gmail.com>
2024-11-04 09:37:58 +0400

    hero client

--- src/app/(web)/page.js
+++ src/app/(web)/page.js
@@ -1,4 +1,3 @@
-// 'use client'
 import Hero from '@/components/Sections/Hero'
 import Services from '@/components/Sections/Services'
 import Portfolio from '@/components/Sections/Portfolio'
@@ -7,52 +6,22 @@ import { initializeSanityClient } from '@/libs/sanity'
 
 const Home = async () => {
   const client = await initializeSanityClient()
-  const data = await client.fetch(
-    `{
-      "heroImages": *[_type == "heroImage"] | order(orderRank) {
-        _id,
-        altText,
-        "imageUrl": image.asset->url
-      },
-      "services": *[_type == "service"] | order(orderRank) {
-        title,
-        description,
-        "imageUrl": image.asset->url,
-        altText
-      }
+
+  const services = await client.fetch(
+    `*[_type == "service"] | order(orderRank){
+      title,
+      description,
+      "imageUrl": image.asset->url,
+      altText
     }`,
-    { next: { revalidate: 3600 } }
+    {
+      next: { revalidate: 3600 },
+    }
   )
-
-  const { heroImages, services } = data
-
-  // const heroImages = await client.fetch(
-  //   `*[_type == "heroImage"] | order(orderRank){
-  //     _id,
-  //     altText,
-  //     "imageUrl": image.asset->url
-  //   }`,
-  //   {
-  //     next: { revalidate: 3600 },
-  //   }
-  // )
-  // console.log('Hero Images: ', heroImages)
-
-  // const services = await client.fetch(
-  //   `*[_type == "service"] | order(orderRank){
-  //     title,
-  //     description,
-  //     "imageUrl": image.asset->url,
-  //     altText
-  //   }`,
-  //   {
-  //     next: { revalidate: 3600 },
-  //   }
-  // )
 
   return (
     <>
-      <Hero images={heroImages} />
+      <Hero />
       <Services services={services} />
       {/* <Portfolio /> */}
       <Contact />
--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -6,26 +6,30 @@ import 'swiper/css/autoplay'
 import { Autoplay, EffectFade } from 'swiper/modules'
 import MidSection from './MidSection'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import { initializeSanityClient } from '@/libs/sanity'
+import { useEffect, useState } from 'react'
 
-const Hero = ({images}) => {
-  // const [images, setImages] = useState([])
-  // console.log(images)
-  // useEffect(() => {
-  //   async function fetchImages() {
-  //     const client = await initializeSanityClient()
-  //     const heroImages = await client.fetch(
-  //       `*[_type == "heroImage"] | order(orderRank){
-  //     _id,
-  //     altText,
-  //     "imageUrl": image.asset->url
-  //   }`
-  //     )
-  //     setImages(heroImages)
-  //   }
-  //   fetchImages()
-  // }, [])
+const Hero = () => {
+  const [images, setImages] = useState(null) // null indicates loading state
+  console.log(images)
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const client = await initializeSanityClient()
+        const heroImages = await client.fetch(
+          `*[_type == "heroImage"] | order(orderRank){
+            _id,
+            altText,
+            "imageUrl": image.asset->url
+          }`
+        )
+        setImages(heroImages)
+      } catch (error) {
+        console.error('Error fetching hero images:', error)
+      }
+    }
+    fetchImages()
+  }, [])
 
   return (
     <div id='/' className='hero-section h-70 w-screen md:h-screen relative'>
@@ -38,18 +42,31 @@ const Hero = ({images}) => {
         loop={true}
         className='md:h-5/6 w-full'
       >
-        {images.map((image) => (
-          <SwiperSlide key={image._id}>
+        {images ? (
+          images.map((image) => (
+            <SwiperSlide key={image._id}>
+              <Image
+                width={100}
+                height={100}
+                unoptimized
+                src={image.imageUrl}
+                alt={image.altText}
+                className='object-cover object-center h-full w-full'
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide>
             <Image
               width={100}
               height={100}
               unoptimized
-              src={image.imageUrl}
-              alt={image.altText}
+              src='/hero.jpg'
+              alt='Hero Image'
               className='object-cover object-center h-full w-full'
             />
           </SwiperSlide>
-        ))}
+        )}
       </Swiper>
       <MidSection />
     </div>
