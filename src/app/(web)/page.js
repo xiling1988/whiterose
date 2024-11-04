6d5b9e2972c8c1f63d684ea2ab99df7e297e6cc5
hans schilling <hasp1988@gmail.com>
2024-11-04 10:43:21 +0400

    service client

--- src/app/(web)/page.js
+++ src/app/(web)/page.js
@@ -5,24 +5,24 @@ import Contact from '@/components/Sections/Contact'
 import { initializeSanityClient } from '@/libs/sanity'
 
 const Home = async () => {
-  const client = await initializeSanityClient()
+  // const client = await initializeSanityClient()
 
-  const services = await client.fetch(
-    `*[_type == "service"] | order(orderRank){
-      title,
-      description,
-      "imageUrl": image.asset->url,
-      altText
-    }`,
-    {
-      next: { revalidate: 3600 },
-    }
-  )
+  // const services = await client.fetch(
+  //   `*[_type == "service"] | order(orderRank){
+  //     title,
+  //     description,
+  //     "imageUrl": image.asset->url,
+  //     altText
+  //   }`,
+  //   {
+  //     next: { revalidate: 3600 },
+  //   }
+  // )
 
   return (
     <>
       <Hero />
-      <Services services={services} />
+      <Services />
       {/* <Portfolio /> */}
       <Contact />
     </>
--- src/components/Sections/Services.js
+++ src/components/Sections/Services.js
@@ -1,24 +1,55 @@
+'use client'
+import { initializeSanityClient } from '@/libs/sanity'
 import Image from 'next/image'
-const Services = ({ services }) => {
-  // console.log(services)
-  // useEffect(() => {
-  //   const fetchServices = async () => {
-  //     try {
-  //       const client = await initializeSanityClient()
-  //       const data = await client.fetch(`*[_type == "service"]{
-  //         title,
-  //         description,
-  //         "imageUrl": image.asset->url,
-  //         altText,
-  //       }`)
-  //       setServices(data)
-  //     } catch (error) {
-  //       console.error('Error fetching services:', error)
-  //     }
-  //   }
+import { useEffect, useState } from 'react'
 
-  //   fetchServices()
-  // }, [])
+const serviceCards = [
+  {
+    title: 'Interior Design',
+    description:
+      'Including material selection, finishes, designs of each space, elevation drawings, MEP drawings, detailed design drawings. Presented with mood boards and 3D renders.',
+    image: '/interior.jpg',
+  },
+  {
+    title: 'Project Management',
+    description:
+      'White Rose will manage the execution of the Interior Design and construction team to ensure all aspects of the design, budget and timeline are managed.',
+    image: '/project.jpg',
+  },
+  {
+    title: 'Landscaping and Swimming Pool Design',
+    description:
+      'Including material selection, finishes, swimming pool design, detailed design drawings. Presented with mood boards and 3D renders.',
+    image: '/pool.jpg',
+  },
+  {
+    title: 'Furniture Procurement and Staging',
+    description:
+      'Furniture selection, purchasing and staging. Management of the deliveries and installation. White Rose Design can also assist with bespoke furniture designs.',
+    image: '/furniture.jpg',
+  },
+]
+const Services = () => {
+  const [services, setServices] = useState(null)
+  console.log(services)
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const client = await initializeSanityClient()
+        const data = await client.fetch(`*[_type == "service"]{
+          title,
+          description,
+          "imageUrl": image.asset->url,
+          altText,
+        }`)
+        setServices(data)
+      } catch (error) {
+        console.error('Error fetching services:', error)
+      }
+    }
+
+    fetchServices()
+  }, [])
 
   return (
     <div id='services' className='bg-canvas bg-fill bg-repeat'>
@@ -33,42 +64,79 @@ const Services = ({ services }) => {
         </p>
 
         <div className='mt-10 space-y-12 md:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0'>
-          {services.map((service) => (
-            <div key={service.title} className='group block mb-4'>
-              <h4 className='md:hidden font-austen text-xl p-1 text-darkCream'>
-                {service.title}
-              </h4>
-              <div
-                aria-hidden='true'
-                className='relative aspect-h-2 aspect-w-3 overflow-hidden rounded-lg lg:aspect-h-6 lg:aspect-w-5'
-              >
-                <Image
-                  unoptimized
-                  height={100}
-                  width={100}
-                  alt={service.title}
-                  src={service.imageUrl}
-                  className='h-full w-full object-cover object-center md:group-hover:opacity-75 md:group-hover:sepia md:group-hover:blur-md transition-all duration-300'
-                />
-                <div className='hidden absolute inset-0  flex-col items-center justify-center p-3 md:group-hover:flex transition-all duration-300'>
-                  <h4 className='font-bold underline text-lg font-austen text-gray-800'>
+          {services
+            ? services.map((service) => (
+                <div key={service.title} className='group block mb-4'>
+                  <h4 className='md:hidden font-austen text-xl p-1 text-darkCream'>
                     {service.title}
                   </h4>
-                  <p className='mt-2 text-md text-center font-bold text-gray-800 '>
-                    {service.description}
-                  </p>
+                  <div
+                    aria-hidden='true'
+                    className='relative aspect-h-2 aspect-w-3 overflow-hidden rounded-lg lg:aspect-h-6 lg:aspect-w-5'
+                  >
+                    <Image
+                      unoptimized
+                      height={100}
+                      width={100}
+                      alt={service.title}
+                      src={service.imageUrl}
+                      className='h-full w-full object-cover object-center md:group-hover:opacity-75 md:group-hover:sepia md:group-hover:blur-md transition-all duration-300'
+                    />
+                    <div className='hidden absolute inset-0  flex-col items-center justify-center p-3 md:group-hover:flex transition-all duration-300'>
+                      <h4 className='font-bold underline text-lg font-austen text-gray-800'>
+                        {service.title}
+                      </h4>
+                      <p className='mt-2 text-md text-center font-bold text-gray-800 '>
+                        {service.description}
+                      </p>
+                    </div>
+                    <p className='md:hidden mt-2 text-lg font-bold text-canvas-3 '>
+                      {service.description}
+                    </p>
+                  </div>
+                  <div className='md:group-hover:hidden'>
+                    <h3 className='hidden underline md:block mt-4 text-base font-semibold text-canvas-3'>
+                      {service.title}
+                    </h3>
+                  </div>
                 </div>
-                <p className='md:hidden mt-2 text-lg font-bold text-canvas-3 '>
-                  {service.description}
-                </p>
-              </div>
-              <div className='md:group-hover:hidden'>
-                <h3 className='hidden underline md:block mt-4 text-base font-semibold text-canvas-3'>
-                  {service.title}
-                </h3>
-              </div>
-            </div>
-          ))}
+              ))
+            : serviceCards.map((service) => (
+                <div key={service.title} className='group block mb-4'>
+                  <h4 className='md:hidden font-austen text-xl p-1 text-darkCream'>
+                    {service.title}
+                  </h4>
+                  <div
+                    aria-hidden='true'
+                    className='relative aspect-h-2 aspect-w-3 overflow-hidden rounded-lg lg:aspect-h-6 lg:aspect-w-5'
+                  >
+                    <Image
+                      unoptimized
+                      height={100}
+                      width={100}
+                      alt={service.title}
+                      src={service.image}
+                      className='h-full w-full object-cover object-center md:group-hover:opacity-75 md:group-hover:sepia md:group-hover:blur-md transition-all duration-300'
+                    />
+                    <div className='hidden absolute inset-0  flex-col items-center justify-center p-3 md:group-hover:flex transition-all duration-300'>
+                      <h4 className='font-bold underline text-lg font-austen text-gray-800'>
+                        {service.title}
+                      </h4>
+                      <p className='mt-2 text-md text-center font-bold text-gray-800 '>
+                        {service.description}
+                      </p>
+                    </div>
+                    <p className='md:hidden mt-2 text-lg font-bold text-canvas-3 '>
+                      {service.description}
+                    </p>
+                  </div>
+                  <div className='md:group-hover:hidden'>
+                    <h3 className='hidden underline md:block mt-4 text-base font-semibold text-canvas-3'>
+                      {service.title}
+                    </h3>
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </div>
