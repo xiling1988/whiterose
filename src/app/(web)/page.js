e3c59a2f3ede09b383c7ff9c91a6f49a46b06575
hans schilling <hasp1988@gmail.com>
2024-11-04 08:27:25 +0400

    combined fetch

--- src/app/(web)/page.js
+++ src/app/(web)/page.js
@@ -5,33 +5,51 @@ import Portfolio from '@/components/Sections/Portfolio'
 import Contact from '@/components/Sections/Contact'
 import { initializeSanityClient } from '@/libs/sanity'
 
-// export const revalidate = 86400 // Revalidate every hour
 
 const Home = async () => {
   const client = await initializeSanityClient()
-  const heroImages = await client.fetch(
-    `*[_type == "heroImage"] | order(orderRank){
-      _id,
-      altText,
-      "imageUrl": image.asset->url
+  const data = await client.fetch(
+    `{
+      "heroImages": *[_type == "heroImage"] | order(orderRank) {
+        _id,
+        altText,
+        "imageUrl": image.asset->url
+      },
+      "services": *[_type == "service"] | order(orderRank) {
+        title,
+        description,
+        "imageUrl": image.asset->url,
+        altText
+      }
     }`,
-    {
-      next: { revalidate: 3600 },
-    }
+    { next: { revalidate: 3600 } }
   )
-  console.log('Hero Images: ', heroImages)
 
-  const services = await client.fetch(
-    `*[_type == "service"] | order(orderRank){
-      title,
-      description,
-      "imageUrl": image.asset->url,
-      altText
-    }`,
-    {
-      next: { revalidate: 3600 },
-    }
-  )
+  const { heroImages, services } = data
+
+  // const heroImages = await client.fetch(
+  //   `*[_type == "heroImage"] | order(orderRank){
+  //     _id,
+  //     altText,
+  //     "imageUrl": image.asset->url
+  //   }`,
+  //   {
+  //     next: { revalidate: 3600 },
+  //   }
+  // )
+  // console.log('Hero Images: ', heroImages)
+
+  // const services = await client.fetch(
+  //   `*[_type == "service"] | order(orderRank){
+  //     title,
+  //     description,
+  //     "imageUrl": image.asset->url,
+  //     altText
+  //   }`,
+  //   {
+  //     next: { revalidate: 3600 },
+  //   }
+  // )
 
   return (
     <>
--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -6,8 +6,27 @@ import 'swiper/css/autoplay'
 import { Autoplay, EffectFade } from 'swiper/modules'
 import MidSection from './MidSection'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { initializeSanityClient } from '@/libs/sanity'
 
-const Hero = ({ images }) => {
+const Hero = ({images}) => {
+  // const [images, setImages] = useState([])
+  // console.log(images)
+  // useEffect(() => {
+  //   async function fetchImages() {
+  //     const client = await initializeSanityClient()
+  //     const heroImages = await client.fetch(
+  //       `*[_type == "heroImage"] | order(orderRank){
+  //     _id,
+  //     altText,
+  //     "imageUrl": image.asset->url
+  //   }`
+  //     )
+  //     setImages(heroImages)
+  //   }
+  //   fetchImages()
+  // }, [])
+
   return (
     <div id='/' className='hero-section h-70 w-screen md:h-screen relative'>
       <Swiper
